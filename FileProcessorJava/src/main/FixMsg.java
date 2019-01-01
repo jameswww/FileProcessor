d752238e3d3f7f794f2ef7fa8cd04cb4e3cf2144
Catherine \( ^_^ )/~~ <Catherine@Yinghongs-MacBook-Pro.local>
2019-01-01 15:18:11 -0500

    first commit

---
+++ FileProcessorJava/src/main/FixMsg.java
@@ -0,0 +1,27 @@
+package main;
+
+public class FixMsg {
+    private int id;
+
+    public String getRawMsg() {
+        return rawMsg;
+    }
+
+    public void setRawMsg(String rawMsg) {
+        this.rawMsg = rawMsg;
+    }
+
+    private String rawMsg;
+
+    public FixMsg() {
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
